8c4b9bc80b3d744d84d15138a8866ea98971fd04
Mats Nord <mnd@qlik.com>
2017-04-24 15:32:41 +0200

    Second Version

--- test/mastermind.kata.js
+++ test/mastermind.kata.js
@@ -3,17 +3,17 @@ import {expect} from 'chai';
 /**
  * I will put my game in the same file as the test
  */
-const EM = "X", IM = "O";
+const EM = "X", IM = "O", NM = "";
 const getGame = (game) =>  game;
 const getMatch = (game, guess) => {
 	let result = "";
 	// Exact Matches
 	guess.forEach(( item, i ) => {
-			result += item === game[i] ? EM : "";
+			result += item === game[i] ? EM : NM;
 	});
 	// Guess is in the set
 	guess.forEach(( item, i ) => {
-			result += item === game[i] ? "" : game.includes( item ) ? IM : ""
+			result += item === game[i] ? NM : game.includes( item ) ? IM : NM;
 	});
 	return result;
 };
@@ -22,12 +22,12 @@ const getMatch = (game, guess) => {
 describe("Master mind game", function(){
 
 	it("should be a game", function() {			
-		const game = getGame([1,2,3,4]);
+		const game = getGame([1,2,4,4]);
 		expect(game).to.be.an('Array');
 	});
 
 	it("should be a set of four numbers", function() {
-		const game = getGame([1,2,3,4]);
+		const game = getGame([1,2,4,4]);
 		expect(game.length).equal(4);
 		game.forEach( item =>{ 
 			 expect(item).to.be.a('Number')
@@ -37,38 +37,48 @@ describe("Master mind game", function(){
 
 	it("should return empty string when no match", function() {
 		// setup
-		const game = getGame([1,1,1,1]);
-		const guess = [4,4,4,4];
+		const game = getGame([1,2,4,4]);
+		const guess = [3,3,3,3];
 		//act
 		const match = getMatch( game, guess );
 		//assert
 		expect(match).equal("");
 	});
 
-	it("should return + when there is an exact match", function() {
+	it("should return X when there is an exact match", function() {
 		// setup
-		const game = getGame([1,1,1,1]);
-		const guess = [1,2,3,4];
+		const game = getGame([1,2,4,4]);
+		const guess = [5,5,5,4];
 		//act
 		const match = getMatch( game, guess );
 		//assert
 		expect(match).equal("X");
 	});
 
-	it( "should return - when there is an number match", function ()	{
+	it( "should return O when there is an number match", function ()	{
 		// setup
-		const game = getGame( [ 1,2,2,2 ] );
-		const guess = [2,3,3,3];
+		const game = getGame( [ 1,2,4,4 ] );
+		const guess = [4,5,5,5];
 		//act
 		const match = getMatch( game, guess );
 		//assert
 		expect( match ).equal( "O" );
 	});
 
+	it( "Exact match overrides in set-match", function ()	{
+		// setup
+		const game = getGame( [1, 2, 4, 4 ] );
+		const guess = [3,2,3,3];
+		//act
+		const match = getMatch( game, guess );
+		//assert
+		expect( match ).equal( "X" );
+	});
+
 	it( "should not reveal position", function ()	{
 		// setup
 		const game = getGame( [ 1,2,3,4 ] );
-		const guess = [4,4,4,4];
+		const guess = [2,3,1,4];
 		//act
 		const match = getMatch( game, guess );
 		//assert
